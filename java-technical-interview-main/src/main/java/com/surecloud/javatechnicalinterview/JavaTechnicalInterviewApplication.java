f98bef9e4db1a6703fc9061473b6287b7a8e6536
Mujahid Al-Okaidi <mujahid.al-okaidi@surecloud.com>
2022-05-24 12:23:53 +0100

    Added code for the pair programming exercise

--- java-technical-interview-main/src/main/java/com/surecloud/javatechnicalinterview/JavaTechnicalInterviewApplication.java
+++ java-technical-interview-main/src/main/java/com/surecloud/javatechnicalinterview/JavaTechnicalInterviewApplication.java
@@ -1,13 +1,29 @@
 package com.surecloud.javatechnicalinterview;
 
+import java.util.List;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
 public class JavaTechnicalInterviewApplication {
 
-	public static void main(String[] args) {
+	public static void main( String[] args) {
 		SpringApplication.run(JavaTechnicalInterviewApplication.class, args);
 	}
 
+	@PostConstruct
+	public void batchProcess()
+	{
+		List<UUID> input = List.of( UUID.fromString( "a70cee93-140e-4451-8940-2da7e5a42f22" ),
+		                            UUID.randomUUID(),
+		                            UUID.fromString( "0c428cfb-34b5-4d57-8627-2f31a59f2abd" ) );
+//		// display all names and scores with these ids
+	}
 }
